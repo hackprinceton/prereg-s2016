da53e4ff895256e67f567f8d3c773dd2bd4399fa
Zachary Liu <liu.zachary@gmail.com>
2016-01-31 04:03:18 -0500

    Add smooth scrolling to navbar links, with correct offset

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@
 
     var bLazy = new Blazy();
 
+    // https://gist.github.com/flesler/3f3e1166690108abf747
+    $("body").on("click", "a[href^='#']", function (e) {
+        e.preventDefault();
+        history.pushState(null, null, this.hash);
+        var offsetTop = -$(".home-nav").height();
+        $(window).stop(true).scrollTo(this.hash, {
+            duration: 500,
+            interrupt: true,
+            offset: {
+                top: offsetTop
+            }
+        });
+    });
+
     function SubscribeForm($form) {
         this._$form = $form;
         this._$input = $form.find("input[type='email']");
